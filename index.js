9aadc94a8c309847bce4de60f0f502efbed1426d
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-29 11:35:17 +0200

    solution team2

--- index.js
+++ index.js
@@ -19,6 +19,35 @@
     ]
  */
 
+/**
+ * Cycle de 3 min
+ * Abdallah -> Christophe -> Gloria -> Kévin -> Oscar -> Abdallah -> ...
+ */
 
 /** Votre code 👇 */
 
+function chess() {
+	const x = ["a", "b", "c", "d", "e", "f", "g", "h"]; //✅
+	const y = ["1", "2", "3", "4", "5", "6", "7", "8"]; //✅
+	let chessboard = []; //✅
+
+	/**
+	 * Cette fonction va créer une ligne de l'echiquier
+	 */
+	for (let i = 0; i < x.length; i++) {
+		//✅
+		//    🚩
+		chessboard.push([]);
+		/**
+		 * Cette fonction va récuperer les valeurs de x et y
+		 * ['a - 1', 'a - 2', 'a - 3', 'a - 4', 'a - 5', 'a - 6', 'a - 7', 'a - 8']
+		 */
+		for (let j = 0; j < y.length; j++) {
+			// ✅
+			chessboard[i].push(x[i] + " - " + y[j]);
+		}
+		//    🚩
+	}
+	return chessboard; //✅
+}
+console.log(chess()); //✅
